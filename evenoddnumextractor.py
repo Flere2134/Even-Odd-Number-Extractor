fac998dfb7c24444b5c94870ec6bcb6196577fe6
Flere21 <aaronhans2134@gmail.com>
2023-04-24 11:44:37 +0800

    split contents into list

--- evenoddnumextractor.py
+++ evenoddnumextractor.py
@@ -2,6 +2,7 @@
 with open("numbers.txt") as my_file:
     content = my_file.read()
 #split content into list of int
+numbers = [int(i) for i in content.split()]
 #create two lists
 #checks each number and sorts them into the list
 #open even txt file and writes the even numbers
