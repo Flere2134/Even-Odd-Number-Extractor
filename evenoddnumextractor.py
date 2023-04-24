ed1fa14d4538a568e96b8e7b785994acab8d43ce
Flere21 <aaronhans2134@gmail.com>
2023-04-24 11:39:35 +0800

    pseudocode

---
+++ evenoddnumextractor.py
@@ -0,0 +1,8 @@
+#open number txt file and read
+with open("numbers.txt") as my_file:
+    content = my_file.read
+#split content into list of int
+#create two lists
+#checks each number and sorts them into the list
+#open even txt file and writes the even numbers
+#open even txt file and writes the odd numbers
