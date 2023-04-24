79d61b6a950a04d7adf222549de25540bf9f6099
Flere21 <aaronhans2134@gmail.com>
2023-04-24 11:46:23 +0800

    empty lists created

--- evenoddnumextractor.py
+++ evenoddnumextractor.py
@@ -4,6 +4,8 @@ with open("numbers.txt") as my_file:
 #split content into list of int
 numbers = [int(i) for i in content.split()]
 #create two lists
+even_numbers = []
+odd_numbers = []
 #checks each number and sorts them into the list
 #open even txt file and writes the even numbers
 #open even txt file and writes the odd numbers
