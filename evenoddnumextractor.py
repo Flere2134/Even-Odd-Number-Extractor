c5c6122a1ae22cf5490df563021fd65aad53101c
Flere21 <aaronhans2134@gmail.com>
2023-04-24 11:41:17 +0800

    open number txt file and reads it

--- evenoddnumextractor.py
+++ evenoddnumextractor.py
@@ -1,6 +1,6 @@
 #open number txt file and read
 with open("numbers.txt") as my_file:
-    content = my_file.read
+    content = my_file.read()
 #split content into list of int
 #create two lists
 #checks each number and sorts them into the list
